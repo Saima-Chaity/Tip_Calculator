f033cc70c2c0c80c5298ee13fe07a635a99f65d8
Saima Chaity <saima.s.chaity@gmail.com>
2019-03-01 20:22:57 -0800

    Added spinner with four options to split the total bill

--- app/src/main/java/com/example/a01040982/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/a01040982/tipcalculator/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.a01040982.tipcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView tipAmount;
     private TextView totalAmount;
     private TextView perPersonAmount;
+
+    private Spinner spinner;
+    private static final String[] items = {"No", "Divided between 2", "Divided between 3", "Divided between 4"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,45 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,items);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+                String totalamount = totalAmount.getText().toString();
+                NumberFormat currency = NumberFormat.getCurrencyInstance();
+                switch (position) {
+                    case 0:
+                        perPersonAmount.setText(currency.format(Double.valueOf(totalamount)));
+                        break;
+                    case 1:
+                        double divideByTwo = Double.valueOf(totalamount);
+                        perPersonAmount.setText(currency.format(divideByTwo / 2  ));
+                        break;
+                    case 2:
+                        double divideByThree = Double.valueOf(totalamount);
+                        perPersonAmount.setText(String.format(currency.format(divideByThree / 3)));
+                        break;
+                    case 3:
+                        double divideByFour = Double.valueOf(totalamount);
+                        perPersonAmount.setText(String.format(currency.format(divideByFour / 4)));
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                String totalamount = totalAmount.getText().toString();
+                perPersonAmount.setText(totalamount);
+            }
+        });
+
 
     }
     public void onPercentageIncrease(){
